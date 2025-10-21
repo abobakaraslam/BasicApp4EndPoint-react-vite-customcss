c6deda0ed44002512b529d10c1c68471e38ea8c1
abobakaraslam <164588250+abobakaraslam@users.noreply.github.com>
2025-10-21 23:11:28 +0500

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+// File: App.jsx located in src folder
+import { useState, useEffect } from "react";
+
+function App() {
+
+const getDataFromApi = async () =>{
+  let myEndpoint = "https://endpoint";
+  let myData = await fetch(myEndpoint);
+  //console.log("received Data: ", myData);
+  let parsedData = await myData.json(); /* Must wait for myData.json */
+  //console.log("parsed received Data: ", parsedData);
+}
+
+  useEffect(() => {
+    getDataFromApi();
+  }, []);
+
+  return (
+    <div>
+      <h1 style={{textAlign: "center"}}>Basic application<br />for<br />Fetching API</h1>
+    </div>
+  );
+}
+ 
+export default App;
